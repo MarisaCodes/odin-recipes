e8d61a9f31b85b5a0e7a95a4ba75408dfb7164b5
MarisaCodes <marisakirisame955@gmail.com>
2022-06-29 18:52:27 +0400

    fixed issues with app.js

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const theFoodImg = document.querySelector('.food-img');
-const recipeLink = document.querySelector('.recipe-link');
-const linkIcon = document.querySelector('.fa-link');
+// const theFoodImg = document.querySelector('.food-img');
+// const recipeLink = document.querySelector('.recipe-link');
+// const linkIcon = document.querySelector('.fa-link');
 const pinTopBar = document.querySelector('.fa-thumbtack');
 const topBar = document.querySelector('#top-bar');
 const pinColor = document.querySelector('.fa-thumbtack');
 
 
-theFoodImg.addEventListener('click', function() {
-    if(theFoodImg.classList.contains('active')){
-            theFoodImg.classList.remove('active');
-            theFoodImg.classList.remove('food-hover');
-            theFoodImg.classList.add('food-img');
-            recipeLink.classList.add('recipe-link');
-        recipeLink.classList.remove('click');
-        linkIcon.classList.add('white-link')
-        linkIcon.classList.remove('blue-link')
-    } else {
-        theFoodImg.classList.remove('food-img');
-        theFoodImg.classList.add('active');
-        recipeLink.classList.remove('recipe-link');
-        recipeLink.classList.add('click');
-        linkIcon.classList.remove('white-link');
-        linkIcon.classList.add('blue-link');
+// theFoodImg.addEventListener('click', function() {
+//     if(theFoodImg.classList.contains('active')){
+//             theFoodImg.classList.remove('active');
+//             theFoodImg.classList.remove('food-hover');
+//             theFoodImg.classList.add('food-img');
+//             recipeLink.classList.add('recipe-link');
+//         recipeLink.classList.remove('click');
+//         linkIcon.classList.add('white-link')
+//         linkIcon.classList.remove('blue-link')
+//     } else {
+//         theFoodImg.classList.remove('food-img');
+//         theFoodImg.classList.add('active');
+//         recipeLink.classList.remove('recipe-link');
+//         recipeLink.classList.add('click');
+//         linkIcon.classList.remove('white-link');
+//         linkIcon.classList.add('blue-link');
 
-    }
-})
+//     }
+// })
 
-theFoodImg.addEventListener('mouseenter', function(){
-    if (theFoodImg.classList.contains('active') !== true) {
-        theFoodImg.classList.add('food-hover');
-    }
-})
+// theFoodImg.addEventListener('mouseenter', function(){
+//     if (theFoodImg.classList.contains('active') !== true) {
+//         theFoodImg.classList.add('food-hover');
+//     }
+// })
 
 
 if(topBar.classList.contains('fix')) {
@@ -45,4 +45,45 @@ pinTopBar.addEventListener('click', function() {
     topBar.classList.remove('fix');
     pinColor.style.color = 'black';
     }
+})
+
+
+const recipeContainer = document.querySelector('div#recipe-card-container');
+const linkIcon = document.querySelectorAll('.fa-link');
+
+
+recipeContainer.addEventListener('click',function(event){
+    if(event.target.classList.contains('food-img') || event.target.classList.contains('food-hover')){
+        if(event.target.classList.contains('active')==false) {
+            event.target.classList.remove('food-img');
+            event.target.classList.add('active');
+            const recipeImg = event.target.parentElement;
+            const aTag = recipeImg.nextElementSibling;
+            const recipeLink = aTag.firstElementChild;
+            const linkIcon = recipeLink.firstElementChild;
+            recipeLink.classList.remove('recipe-link');
+            recipeLink.classList.add('click');
+            linkIcon.classList.remove('white-link');
+            linkIcon.classList.add('blue-link');
+
+
+
+            if(!event.target.classList.contains('food-hover')){
+                event.target.classList.add('food-hover'); 
+            }
+        } else {
+            event.target.classList.remove('active');
+            event.target.classList.remove('food-hover');
+            event.target.classList.add('food-img');
+            const recipeImg = event.target.parentElement;
+            const aTag = recipeImg.nextElementSibling;
+            const recipeLink = aTag.firstElementChild;
+            const linkIcon = recipeLink.firstElementChild;
+            linkIcon.classList.add('white-link');
+            linkIcon.classList.remove('blue-link');
+            recipeLink.classList.add('recipe-link');
+            recipeLink.classList.remove('click');
+            }
+
+    } 
 })
